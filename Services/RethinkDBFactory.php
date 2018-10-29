fc70d53bddfa94db24ace2e557fbbac61a848cde
ABRAHAM Morgan <morgan@tramicms.net>
2018-10-29 17:12:08 +0100

    Update Services/RethinkDBFactory.php

--- Services/RethinkDBFactory.php
+++ Services/RethinkDBFactory.php
@@ -6,14 +6,8 @@ use Geekimo\Bundle\RethinkDBBundle\Connection;
 
 class RethinkDBFactory
 {
-    private $instance = null;
-
     static public function getConnection($parameters) : Connection
     {
-        if($this->instance instanceof Connection) {
-            return $this->instance;
-        } else {
-            return $this->instance = new Connection($parameters);
-        }
+        return new Connection($parameters);
     }
 }
