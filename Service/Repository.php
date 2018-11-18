edcbcdf8d02c9581afc40a612a42113dfd1eb472
ABRAHAM Morgan <morgan@tramicms.net>
2018-11-18 15:09:24 +0100

    DI addition
    Adding Symfony ContainerInterface as DI to make the models be able to use services.

--- Service/Repository.php
+++ Service/Repository.php
@@ -2,6 +2,8 @@
 
 namespace Geekimo\Bundle\RethinkDBBundle\Service;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 use Geekimo\Bundle\RethinkDBBundle\Exception\RepositoryErrorException;
 use Geekimo\Bundle\RethinkDBBundle\ModelBase;
 use Geekimo\Bundle\RethinkDBBundle\Service\Connection;
@@ -9,10 +11,12 @@ use Geekimo\Bundle\RethinkDBBundle\Service\Connection;
 class Repository
 {
     private $connection;
+    private $container;
 
-    public function __construct(Connection $connection)
+    public function __construct(Connection $connection, ContainerInterface $container)
     {
         $this->connection = $connection;
+        $this->container  = $container;
     }
 
     public function get(string $model) {
@@ -22,7 +26,10 @@ class Repository
             throw new RepositoryErrorException(sprintf('Model "%s" must extend "%s"', get_class($model), ModelBase::class));
         }
 
+        // some DI
         $model->setConnection($this->connection);
+        $model->setContainer($this->container);
+
         return $model;
     }
 }
