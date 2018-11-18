e9f0e6152235be2699a599aab3cca46158cd3b3c
ABRAHAM Morgan <morgan@tramicms.net>
2018-11-18 21:15:46 +0100

    Update ModelBase.php

--- ModelBase.php
+++ ModelBase.php
@@ -2,14 +2,21 @@
 
 namespace Geekimo\Bundle\RethinkDBBundle;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 use Geekimo\Bundle\RethinkDBBundle\Service\Connection;
 
 class ModelBase
 {
-    protected $protection;
+    protected $connection;
     
     public function setConnection(Connection $connection)
     {
         $this->connection = $connection;
+    }
+    
+    public function setContainer(ContainerInterface $container)
+    {
+        $this->container = $container;
     }
 }
