c4f74377dd29f2c47f1129ad39428555a5ddf84c
Morgan Abraham <morgan@geekimo.me>
2024-09-22 14:11:00 +0200

    Fix configuration load

--- src/RethinkDBBundle.php
+++ src/RethinkDBBundle.php
@@ -1,8 +1,26 @@
 <?php
 namespace Geekimo\Bundle\RethinkDBBundle;
 
+use Geekimo\Bundle\RethinkDBBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class RethinkDBBundle extends AbstractBundle
+final class RethinkDBBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $config = (new Processor())->processConfiguration(new Configuration(), $config);
+
+        $loader = new YamlFileLoader(
+            $builder,
+            new FileLocator(__DIR__ . '/Resources/config')
+        );
+        $loader->load('services.yml');
+
+        $builder->setParameter('rethink_db', $config);
+    }
 }
